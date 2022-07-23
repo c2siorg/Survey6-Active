28719eb3d8e789338a6a7c353ca8ba13cd5a0fbd
1209973 <e.chamath@yahoo.com>
2022-07-23 08:27:22 +0530

    feature : generate IPv6 addresses for a given network range

--- src/scan.cpp
+++ src/scan.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <survery6_active/scan.h>
 
+ip::address_v6_range generate_ip_addresses(string network_range){
+    boost::system::error_code error_code;
+    ip::network_v6 subnet = ip::make_network_v6(network_range,error_code);
+    if(error_code)
+    {
+        std::cerr << "Error in the network range argument: " <<error_code.message() << "\n";
+    }
+    ip::address_v6_range hosts = subnet.hosts();
+
+    return hosts;
+}
+
 // TODO:
-// generates candidate addresses
 // scans for them
 // tests the network ranges where live addresses are found for aliased conditions
 // add legitimate discovered IPv6 addresses to an output list
-
 void scan(string network)
 {
     cout << "Scanning networking rage " << network << endl;
+    ip::address_v6_range hosts= generate_ip_addresses(network);
+    for (auto addrs: hosts){
+        string ip6 = addrs.to_string();
+        cout << ip6 <<endl;
+    }
     cout << "Scan completed" << endl;
+
 }
